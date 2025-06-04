3669667fb2af2543f0b85b0bafc00694cfc9f1ec
Rayo-Dev10 <134890134+Rayo-Dev10@users.noreply.github.com>
2025-06-04 06:26:02 -0500

    Revise codes and modal

--- update_codes.py
+++ update_codes.py
@@ -1,58 +1,24 @@
 import json
-import unicodedata
 
-files=['administracion.json','contaduria.json']
-all_data={}
-for file in files:
-    with open(file, 'r', encoding='utf8') as f:
-        all_data[file]=json.load(f)
+files=[('administracion.json','ADMI'),('contaduria.json','CONTA')]
 
-# Normalize name to remove spaces and accents
-def normalize(name):
-    nfkd=unicodedata.normalize('NFD', name)
-    cleaned=''.join(c for c in nfkd if unicodedata.category(c)!='Mn')
-    return cleaned.replace(' ','').replace('-','')
-
-used=set()  # store codes without prefix
-old_to_new={}
-
-for file in files:
-    prefix='ADMI' if file=='administracion.json' else 'CONTA'
-    for subs in all_data[file].values():
-        for sub in subs:
-            base=normalize(sub['nombre'])
-            credits=sub['creditos']
-            left=4
-            right=0
-            body=base[:left]
-            code_no_prefix=f"{body}C{credits}"
-            if code_no_prefix in used:
-                right=4
-                body=base[:left]+base[-right:]
-                code_no_prefix=f"{body}C{credits}"
-                while code_no_prefix in used and left+right < len(base):
-                    left=min(left+2, len(base))
-                    body=base[:left]+base[-right:]
-                    code_no_prefix=f"{body}C{credits}"
-                idx=1
-                unique=code_no_prefix
-                while unique in used:
-                    idx+=1
-                    unique=f"{code_no_prefix}{idx}"
-                code_no_prefix=unique
-            used.add(code_no_prefix)
-            code=f"{prefix}-{code_no_prefix}"
+for fname,prefix in files:
+    with open(fname,'r',encoding='utf8') as f:
+        data=json.load(f)
+    old_to_new={}
+    counter=1
+    # Assign new sequential codes
+    for sem in sorted(data.keys(), key=lambda x:int(x)):
+        for sub in data[sem]:
+            code=f"{prefix}-{counter:03d}C{sub['creditos']}"
             old_to_new[sub['code']]=code
             sub['code']=code
-
-# Update prerequisites to new codes
-for file in files:
-    for subs in all_data[file].values():
-        for sub in subs:
+            counter+=1
+    # Update prerequisites
+    for sem in data:
+        for sub in data[sem]:
             pre=sub.get('pre-requisite')
             if pre in old_to_new:
                 sub['pre-requisite']=old_to_new[pre]
-
-for file in files:
-    with open(file,'w',encoding='utf8') as f:
-        json.dump(all_data[file], f, ensure_ascii=False, indent=2)
+    with open(fname,'w',encoding='utf8') as f:
+        json.dump(data,f,ensure_ascii=False,indent=2)

1795851c668430da2c3db4bed31f5bfde6d73b8c
Rayo-Dev10 <134890134+Rayo-Dev10@users.noreply.github.com>
2025-06-04 08:18:34 -0500

    refactor: improve modularity and efficiency

--- update_codes.py
+++ update_codes.py
@@ -1,24 +1,31 @@
 import json
 
-files=[('administracion.json','ADMI'),('contaduria.json','CONTA')]
 
-for fname,prefix in files:
-    with open(fname,'r',encoding='utf8') as f:
-        data=json.load(f)
-    old_to_new={}
-    counter=1
-    # Assign new sequential codes
-    for sem in sorted(data.keys(), key=lambda x:int(x)):
+def update_file(fname, prefix):
+    with open(fname, 'r', encoding='utf8') as f:
+        data = json.load(f)
+    old_to_new = {}
+    counter = 1
+    for sem in sorted(data.keys(), key=lambda x: int(x)):
         for sub in data[sem]:
-            code=f"{prefix}-{counter:03d}C{sub['creditos']}"
-            old_to_new[sub['code']]=code
-            sub['code']=code
-            counter+=1
-    # Update prerequisites
+            code = f"{prefix}-{counter:03d}C{sub['creditos']}"
+            old_to_new[sub['code']] = code
+            sub['code'] = code
+            counter += 1
     for sem in data:
         for sub in data[sem]:
-            pre=sub.get('pre-requisite')
+            pre = sub.get('pre-requisite')
             if pre in old_to_new:
-                sub['pre-requisite']=old_to_new[pre]
-    with open(fname,'w',encoding='utf8') as f:
-        json.dump(data,f,ensure_ascii=False,indent=2)
+                sub['pre-requisite'] = old_to_new[pre]
+    with open(fname, 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def main():
+    files = [('administracion.json', 'ADMI'), ('contaduria.json', 'CONTA')]
+    for fname, prefix in files:
+        update_file(fname, prefix)
+
+
+if __name__ == '__main__':
+    main()
